7cf31ab994fa60e085c431994ba4ff5d3d59a887
Narender <narendersinghsep1997@gmail.com>
2022-11-14 13:29:20 +0530

    Third Commit

--- src/services/auth.service.jsx
+++ src/services/auth.service.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const API_URL = "https://tomcat1.shiftescape.com/api/auth/";
 
+//const API_URL = "http://localhost:8080/api/auth"
+
 const login = (email, password) => {
   return axios
     .post(API_URL + "signin", {
