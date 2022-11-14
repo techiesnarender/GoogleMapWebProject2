8b1221469a3707a64f274d054e432401e5e52ae0
Narender <narendersinghsep1997@gmail.com>
2022-11-14 15:26:57 +0530

    Sixth commit

--- src/services/UserServices.jsx
+++ src/services/UserServices.jsx
@@ -2,7 +2,8 @@ import http from "../http-common";
 import authHeader from "./auth-header";
 
 const getAll = () => {
-  return http.get("/admin/users", { headers: authHeader() });
+  return http.get("/users", { headers: authHeader() });
+ // return http.get("/admin/users", { headers: authHeader() });
 };
 
 const get = id => {
